94a25da7b59f563baf864f0649e82634eca7c6dc
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2025-09-05 21:40:52 -0300

    feat: add estrutura inicial

---
+++ usuario-service/src/test/java/br/com/lcano/usuario_service/UsuarioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lcano.usuario_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
