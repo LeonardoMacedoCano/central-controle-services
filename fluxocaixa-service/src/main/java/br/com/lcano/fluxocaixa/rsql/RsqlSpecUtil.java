20b4594cf612481db7f06945816db65a046aff6c
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2026-01-25 19:17:43 -0300

    fix: corrigir bug filtro tipo boolean

--- fluxocaixa-service/src/main/java/br/com/lcano/fluxocaixa/rsql/RsqlSpecUtil.java
+++ fluxocaixa-service/src/main/java/br/com/lcano/fluxocaixa/rsql/RsqlSpecUtil.java
@@ -11,16 +11,18 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class RsqlSpecUtil {
+
+    private static final Set<ComparisonOperator> OPERATORS =
+            Arrays.stream(RsqlSearchOperation.values())
+                    .map(RsqlSearchOperation::toOperator)
+                    .collect(Collectors.toCollection(HashSet::new));
+
     public static <T> Specification<T> fromFilter(String filter) {
         if (filter == null || filter.isBlank()) {
             return null;
         }
 
-        Set<ComparisonOperator> operators = Arrays.stream(RsqlSearchOperation.values())
-                .map(RsqlSearchOperation::toOperator)
-                .collect(Collectors.toCollection(HashSet::new));
-
-        RSQLParser parser = new RSQLParser(operators);
+        RSQLParser parser = new RSQLParser(OPERATORS);
         Node rootNode = parser.parse(filter);
 
         RsqlSpecificationBuilder<T> builder = new RsqlSpecificationBuilder<>();
--- fluxocaixa-service/src/main/java/br/com/lcano/fluxocaixa/rsql/RsqlSpecification.java
+++ fluxocaixa-service/src/main/java/br/com/lcano/fluxocaixa/rsql/RsqlSpecification.java
@@ -1,5 +1,6 @@
 package br.com.lcano.fluxocaixa.rsql;
 
+import jakarta.annotation.Nonnull;
 import jakarta.persistence.criteria.*;
 import org.springframework.data.jpa.domain.Specification;
 
@@ -14,19 +15,77 @@ public class RsqlSpecification<T> implements Specification<T> {
     }
 
     @Override
-    public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
-        String value = criteria.getValues().getFirst();
+    public Predicate toPredicate(
+            Root<T> root,
+            @Nonnull CriteriaQuery<?> query,
+            @Nonnull CriteriaBuilder builder
+    ) {
+
+        Path<?> path = root.get(criteria.getProperty());
+        Class<?> fieldType = path.getJavaType();
+
+        String rawValue = criteria.getValues().getFirst();
+        Object value = convertValue(rawValue, fieldType);
 
         return switch (criteria.getOperation()) {
-            case EQUAL -> builder.equal(root.get(criteria.getProperty()), value);
-            case NOT_EQUAL -> builder.notEqual(root.get(criteria.getProperty()), value);
-            case GREATER_THAN -> builder.greaterThan(root.get(criteria.getProperty()), value);
-            case LESS_THAN -> builder.lessThan(root.get(criteria.getProperty()), value);
-            case GREATER_THAN_OR_EQUAL -> builder.greaterThanOrEqualTo(root.get(criteria.getProperty()), value);
-            case LESS_THAN_OR_EQUAL -> builder.lessThanOrEqualTo(root.get(criteria.getProperty()), value);
-            case IN -> root.get(criteria.getProperty()).in(criteria.getValues());
-            case NOT_IN -> builder.not(root.get(criteria.getProperty()).in(criteria.getValues()));
-            case LIKE -> builder.like(builder.lower(root.get(criteria.getProperty())), "%" + value.toLowerCase(Locale.ROOT) + "%");
+            case EQUAL -> builder.equal(path, value);
+            case NOT_EQUAL -> builder.notEqual(path, value);
+
+            case GREATER_THAN -> greaterThan(builder, path, value);
+            case LESS_THAN -> lessThan(builder, path, value);
+            case GREATER_THAN_OR_EQUAL -> greaterThanOrEqual(builder, path, value);
+            case LESS_THAN_OR_EQUAL -> lessThanOrEqual(builder, path, value);
+
+            case IN -> path.in(criteria.getValues());
+            case NOT_IN -> builder.not(path.in(criteria.getValues()));
+
+            case LIKE -> builder.like(
+                    builder.lower(path.as(String.class)),
+                    "%" + rawValue.toLowerCase(Locale.ROOT) + "%"
+            );
         };
+    }
+
+    private Object convertValue(String value, Class<?> fieldType) {
+        if (fieldType == Boolean.class || fieldType == boolean.class) {
+            return Boolean.valueOf(value);
+        }
+        return value;
+    }
+
+    @SuppressWarnings("unchecked")
+    private <Y extends Comparable<? super Y>> Predicate greaterThan(
+            CriteriaBuilder builder,
+            Path<?> path,
+            Object value
+    ) {
+        return builder.greaterThan((Expression<Y>) path, (Y) value);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <Y extends Comparable<? super Y>> Predicate lessThan(
+            CriteriaBuilder builder,
+            Path<?> path,
+            Object value
+    ) {
+        return builder.lessThan((Expression<Y>) path, (Y) value);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <Y extends Comparable<? super Y>> Predicate greaterThanOrEqual(
+            CriteriaBuilder builder,
+            Path<?> path,
+            Object value
+    ) {
+        return builder.greaterThanOrEqualTo((Expression<Y>) path, (Y) value);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <Y extends Comparable<? super Y>> Predicate lessThanOrEqual(
+            CriteriaBuilder builder,
+            Path<?> path,
+            Object value
+    ) {
+        return builder.lessThanOrEqualTo((Expression<Y>) path, (Y) value);
     }
 }
